71884d67d3b14819eff31347fa7b799a4c73affd
burgermori0 <horomori0@gmail.com>
2026-07-29 21:40:10 -0400

    Update bcsfe.ps1

--- bcsfe.ps1
+++ bcsfe.ps1
@@ -3,3 +3,6 @@ python -m pip install -U git+https://codeberg.org/fieryhenry/BCSFE-Python.git
 
 Write-Host "Launching BCSF" -ForegroundColor Green
 python -m bcsfe
+
+Write-Host 'bcsfe is fully loaded, if its not loaded then type "bcsfe" into your terminal' -ForegroundColor Yellow
+
